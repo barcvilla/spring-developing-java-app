8a5ae4f41de60d0e6314e5a8a154a405aaa59cdd
Carlos Villanueva <ceva_19@hotmail.com>
2018-11-23 16:16:38 -0300

    Building Spring MVC Web App

--- module2/1/webstore/src/main/java/com/packt/webstore/config/WebApplicationContextConfig.java
+++ module2/1/webstore/src/main/java/com/packt/webstore/config/WebApplicationContextConfig.java
@@ -5,6 +5,7 @@
  */
 package com.packt.webstore.config;
 
+import javax.faces.application.ResourceHandler;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,6 +14,7 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -58,6 +60,16 @@ public class WebApplicationContextConfig implements WebMvcConfigurer{
         urlPathHelper.setRemoveSemicolonContent(false);
         
         configurer.setUrlPathHelper(urlPathHelper);
+    }
+    
+    /**
+     * Declarando recursos estaticos (images)
+     * @param registry 
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        registry.addResourceHandler("/img/**").addResourceLocations("/resources/images/");
     }
     
     @Bean
