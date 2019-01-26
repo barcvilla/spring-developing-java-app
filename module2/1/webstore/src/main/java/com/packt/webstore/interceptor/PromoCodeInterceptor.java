d123706e8de3e8419b5099fe8cd0886c1160378c
Carlos Villanueva <ceva_19@hotmail.com>
2019-01-26 01:26:02 -0300

    Building Java Web App with Spring Framework 5 - Modulo 2 - Working with Intercerptos and Mapped Interceptors

--- module2/1/webstore/src/main/java/com/packt/webstore/interceptor/PromoCodeInterceptor.java
+++ module2/1/webstore/src/main/java/com/packt/webstore/interceptor/PromoCodeInterceptor.java
@@ -26,13 +26,30 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  * usuarios que posean un codigo valido de promocion, pero los otros que intentan acceder a esta oferta especial
  * de productos con un codigo de promocion invalido deberian ser direccionados a una pagina de error. Para este ejemplo
  * usaremos Mapped Interceptors
+ * 
+ * La clase PromoCodeInterceptor es similar a la clase ProcessingTimeLogInterceptor, la unica diferencia es que se ha
+ * extendido HandlerInterceptorAdapter y sobreescrito el metodo preHandle
  * @author PC
  */
 public class PromoCodeInterceptor extends HandlerInterceptorAdapter {
+    /**
+     * usado para configurar el codigo valido de promocion, en este caso sera OFF3R, asi cualquiera que acceda al
+     * a la pagina de oferta de productos debera ingresa el codigo OFF3R como codigo valido
+    */
     private String promoCode;
+    /**
+     * Estos dos atributos son usados para la redireccion, errorRedirect indica el URL mapping redireccionado en caso de
+     * un codigo de promocion invalido y offerRedirect indica el URL mapping redireccionado para un codigo de promocion
+     * valido
+     */
     private String errorRedirect;
     private String offerRedirect;   
     
+    /**
+     * Verificamos si el request contiene el codigo de promocion correcto como parametro HTTP
+     * si es asi, redirigimos el request a la pagina configurada como specialoffert, en otro caso
+     * a la pagina configurada de error
+     */
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException
     {
         String givenPromoCode = request.getParameter("promo");
